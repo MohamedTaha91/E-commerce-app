684a04e721c76a1c3b6d280f04df054dd8417a8b
Florian Cartier <florian.cartier@hotjar.com>
2024-02-06 18:44:44 +0100

    ECOM-1: Remove global styles and fix page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,8 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { sql } from "@vercel/postgres";
-  
 
-const likes = 100;
-export default async function Home() {
-  const deleteDb = async() => {
-    const { rows } = await sql`
-      DROP TABLE products
-    `;
-  }
-
-  const createTable = async() => {
-    const { rows } = await sql`
-      CREATE TABLE IF NOT EXISTS products (
-        id SERIAL PRIMARY KEY,
-        title VARCHAR(255) NOT NULL,
-        price FLOAT NOT NULL,
-        image VARCHAR(255)
-      );
-    `;
-  }
-
-  const insertProduct = async() => {
-    const { rows } = await sql`
-      INSERT INTO products(title, price, image) VALUES ('MantaRay', 10.0, 'https://i.etsystatic.com/21493117/r/il/a18e30/3392715649/il_fullxfull.3392715649_630p.jpg' )
-    `;
-  }
-
- //await deleteDb()
- //await createTable()
- //await insertProduct()
-  
-
- 
+export default function Home() {
+    return (
+        <h1>Home Page</h1>
+    );
 }
